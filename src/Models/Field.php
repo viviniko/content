429b0beb94b5303207682f536c1581ea296c9681
viviniko <viviniko.shop@gmail.com>
2019-10-06 11:28:05 +0800

    :trollfacee:

--- src/Models/Field.php
+++ src/Models/Field.php
@@ -8,10 +8,11 @@ class Field extends Model
 {
     protected $tableConfigKey = 'content.fields_table';
 
-    protected $fillable = ['model_id', 'name', 'display_name', 'description', 'is_required', 'input_type', 'input_data'];
+    protected $fillable = ['model_id', 'name', 'display_name', 'description', 'is_required', 'is_display', 'input_type', 'input_data'];
 
     protected $casts = [
         'is_required' => 'boolean',
+        'is_display' => 'boolean',
     ];
 
     public function model()
